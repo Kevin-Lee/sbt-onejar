4397c594c8173848989247e7f73b606e12fdf810
Kevin Lee <kevin.code@lckymn.com>
2015-02-16 18:41:11 +1100

    Fixed: #1 - IllegalStateException: Fatal: Failed to load the internal config for AWS Java SDK https://github.com/Kevin-Lee/sbt-onejar/issues/1 - Simple fix for #1 (using one-jar-boot-0.97-ex1) - This branch is for sbt 0.13

--- project/build.scala
+++ project/build.scala
@@ -2,16 +2,51 @@ import sbt._
 import Keys._
 
 object build extends Build {
-  val sbtXjc = Project(
-    id = "sbt-onejar",
+
+//  val sbtXjc = Project(
+//    id = "sbt-onejar",
+//    base = file("."),
+//    settings = Defaults.defaultSettings ++ ScriptedPlugin.scriptedSettings ++ Seq[Project.Setting[_]](
+//      organization := "org.scala-sbt.plugins",
+//      version := "0.9-SNAPSHOT",
+//      sbtPlugin := true,
+//      scalacOptions in Compile ++= Seq("-deprecation"),
+//      publishTo := Some(Resolver.url("sbt-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)),
+//      publishMavenStyle := false
+//    )
+//  )
+
+  val nexus = "http://nexus.lckymn.com/"
+
+  val sbtPublicDeploy = Project(
+    id = "sbt-onejar-ex",
     base = file("."),
     settings = Defaults.defaultSettings ++ ScriptedPlugin.scriptedSettings ++ Seq[Project.Setting[_]](
       organization := "org.scala-sbt.plugins",
-      version := "0.9-SNAPSHOT",
+      version := "0.8.1",
       sbtPlugin := true,
       scalacOptions in Compile ++= Seq("-deprecation"),
-      publishTo := Some(Resolver.url("sbt-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)),
-      publishMavenStyle := false
+      publishTo <<= version { (v: String) =>
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/kevin-public-snapshots")
+        else
+          Some("releases"  at nexus + "content/repositories/kevin-public-releases")
+      },
+      publishMavenStyle := true,
+      credentials += Credentials(Path.userHome / ".ivy2" / ".nexus-deploy-credentials"),
+      pomIncludeRepository := { _ => false },
+      pomExtra := (
+        <url>https://github.com/Kevin-Lee/sbt-onejar</url>
+          <licenses>
+            <license>
+              <name>The MIT License</name>
+              <url>https://raw.githubusercontent.com/Kevin-Lee/sbt-onejar/master/LICENSE</url>
+            </license>
+          </licenses>
+          <scm>
+            <url>git@github.com:Kevin-Lee/sbt-onejar.git</url>
+            <connection>scm:git:git@github.com:Kevin-Lee/sbt-onejar.git</connection>
+          </scm>)
     )
   )
 }
